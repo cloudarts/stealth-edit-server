ff785a941c8bb822aba2338bf34c6317c829b261
Wojciech Andreas Jurczyk <wajurczyk@gmail.com>
2013-02-21 22:41:00 +0100

    added basic POST handling. nothing's done with the data right now

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+
 app.get('/*', function(req, res){
 	var body = 'Hello World';
 	res.end(body);
+});
+
+app.post('/saveLevel', function(req, res){
+  console.log(req.body);
+  res.send("ok");
 });
 
 app.listen(88);
