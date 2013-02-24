30ca9a908fab38856bc29ada653434a05dd64362
Wojciech Andreas Jurczyk <wajurczyk@gmail.com>
2013-02-24 11:46:52 +0100

    added backup feature for overwriting files. bit better console logging

--- server.js
+++ server.js
@@ -5,22 +5,23 @@ var util = require('util');
 
 app.use(require('connect').bodyParser());
 
-/*
-app.get('/*', function(req, res){
-	var body = 'Hello World';
-	res.end(body);
-});
-*/
 
 app.post('/saveLevel', function(req, res){
   	var levelObject = req.body;
-	console.log(util.inspect(levelObject));
+	console.log("**************\nattempting to save: \n" + util.inspect(levelObject) + "\n\n");
+
+	// backup old file
+	if( fs.existsSync(levelObject.levelID ) ) {
+		fs.renameSync(levelObject.levelID, levelObject.levelID + "_" + new Date().getTime());
+	}
+
+	// write new level object file
 	fs.writeFile(levelObject.levelID, JSON.stringify(levelObject), function(err) {
     		if(err) {
-        		console.log(err);
+        		console.log(err + "\n\n");
 			res.send("error");
     		} else {
-        		console.log("" + levelObject.levelID + " saved!");
+        		console.log("" + levelObject.levelID + " saved!\n\n");
 			res.send("ok");
     		}
 	}); 
@@ -28,13 +29,15 @@ app.post('/saveLevel', function(req, res){
 
 app.get('/loadLevel/:levelID', function(req, res) {
 	var levelID = req.params.levelID;
+	console.log("**************\nattempting to load:\n" + levelID);
+
 	fs.readFile(levelID, 'utf8', function(err, data) {
 		if( err ) {
-			console.log(err);
+			console.log(err + "\n\n");
 			res.send("error");
 		}
 		else {
-			console.log("loaded: " + util.inspect(data));
+			console.log("loaded: " + util.inspect(data) + "\n\n");
 			
 			res.send(data);
 		}
@@ -42,4 +45,4 @@ app.get('/loadLevel/:levelID', function(req, res) {
 });
 
 app.listen(88);
-console.log("server running");
+console.log("\nserver running on port 88\n\n");
